eb99a049f7120d1cb57b02c0829a198cc2326632
mumbaiops45 <mumbaiops45@gmail.com>
2026-07-14 10:59:24 +0530

    latest

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   keywords:
     "Balvir Lifting, elevator accessories, wire ropes, KISWIRE, CEDES, Fermator, Wittur, LED lighting, flat travelling cables, industrial automation, Navi Mumbai",
       icons: {
-    icon: "/logo.jpeg",
+    icon: "/newlogo.jpeg",
   },
 };
 
